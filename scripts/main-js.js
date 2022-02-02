95e3df31e4a373fbc9ce028c19f8eea5df614f4d
Revixxd <49001614+Revixxd@users.noreply.github.com>
2022-02-02 21:01:22 +0100

    adding some visual things
    -adding stylesheet

--- scripts/main-js.js
+++ scripts/main-js.js
@@ -1,10 +1,15 @@
+// const container = document.getElementById('container');
 const input = document.getElementById('input')
 const list = document.getElementById('list')
+const buttonC = document.getElementById('clear-all')
+
+const div = document.getElementsByClassName('paragraph-hover')
 
 input.addEventListener('keydown', ({key})=>{
+    
     if(key === 'Enter'){
         if(list.getElementsByTagName('div').length == 8){
-            alert('dupa')
+            alert('limit listy został uzyskany')
             input.value = '';
         }else{
             if(input.value !== ''){
@@ -14,16 +19,27 @@ input.addEventListener('keydown', ({key})=>{
                 paragraph.innerText = input.value
     
                 list.appendChild(div)
-    
                 div.appendChild(paragraph)
-    
+                
+                div.classList.add('paragraph')
                 input.value = '';
+
+                div.addEventListener('click', ()=>{
+                    list.removeChild(div)
+                })
+
+                buttonC.addEventListener('click', ()=>{
+                    list.removeChild(div)
+                })
+
             }else{
                 alert('pole jest puste!')
             }
-        }
-         
+        } 
     }
     
+    
 })
+
+
 
